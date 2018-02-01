b1dd4adede6d435ca5414cff3fbeb169ce582a55
lindelani <lindelanixhanti@gmail.com>
2018-02-01 15:53:54 +0200

    My tour app

---
+++ app/src/main/java/com/map/lindelani/mptourism/MainActivity.java
@@ -0,0 +1,41 @@
+package com.map.lindelani.mptourism;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayout hotelLinear;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButton();
+
+    }
+    public void addListenerOnButton(){
+
+        final Context context=this;
+        hotelLinear = (LinearLayout) findViewById(R.id.hotel);
+
+        hotelLinear.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public  void onClick(View view){
+
+                Intent intent = new Intent(context, Hotel.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+        //button 2
+
+    }
+}
